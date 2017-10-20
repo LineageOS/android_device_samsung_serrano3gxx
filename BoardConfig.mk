dedae4ccc335a9a5f7fc2d0900cc84741c29a778
Arne Coucheron <arco68@gmail.com>
2017-10-21 01:42:02 +0200

    serrano3gxx: Remove support for BLN
    This will not be supported going forward.
    
    Change-Id: I6505bead78bb020abcf008ab8fbacb37c9161619

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,8 +36,5 @@ TARGET_KERNEL_VARIANT_CONFIG := msm8930_serrano_eur_3g_defconfig
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# BLN
-BOARD_HAVE_GENERIC_BLN := true
-
 # Charger
 BOARD_NO_CHARGER_LED := true
